784d0c59448bd95f4d976e92bcf75ba762a60821
Maycon Aguiar <maycon.aguiar@somosphi.com>
2021-01-23 13:46:09 -0300

    pass to another branch

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: ENV_SILENT=true yarn knex migrate:latest --force
+web: ENV_SILENT=true yarn dev
--- src/util/env.ts
+++ src/util/env.ts
@@ -6,13 +6,11 @@ const env = {
   /* Http Interface Config */
   httpPort: parseInt(process.env.HTTP_PORT || '', 10),
   httpBodyLimit: process.env.HTTP_BODY_LIMIT,
-
-  /* Mysql Adapter Config */
   mysqlPort: parseInt(process.env.MYSQL_PORT || '', 10),
-  mysqlHost: process.env.MYSQL_HOST || 'localhost',
-  mysqlUser: process.env.MYSQL_USER || 'root',
-  mysqlPassword: process.env.MYSQL_PASSWORD || '12345',
-  mysqlSchema: process.env.MYSQL_SCHEMA || 'challenge',
+  mysqlHost: process.env.MYSQL_HOST || 'us-cdbr-east-03.cleardb.com',
+  mysqlUser: process.env.MYSQL_USER || 'b6e24acdc47cce',
+  mysqlPassword: process.env.MYSQL_PASSWORD || '44384c06',
+  mysqlSchema: process.env.MYSQL_SCHEMA || 'heroku_9ac7685e83afe1f',
   mysqlDebug: process.env.MYSQL_DEBUG === 'true',
   mysqlPoolMin: parseInt(process.env.MYSQL_POOL_MIN || '0', 10),
   mysqlPoolMax: parseInt(process.env.MYSQL_POOL_MIN || '1', 10),
